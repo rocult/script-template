95963d4d00046d0a3f3ffa74f15fe7916295d969
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2025-05-30 21:11:09 +0100

    move global types to a dependency

--- README.md
+++ README.md
@@ -13,4 +13,5 @@ Please read the documentation on how to best use this template and installation.
 1. Make sure you install [pesde](https://docs.pesde.dev/installation/) **exactly** how the [documentation describes here](https://docs.pesde.dev/installation/)
 2. Make sure you install lune 0.8.9 (**not 0.9.\***) [from here](https://github.com/lune-org/lune/releases/tag/v0.8.9)
 3. Run `pesde install` while `cd` in this (root) directory
+   1. \[Optional\] Reload Luau Language Server (`CTRL + P` and search for it)
 4. Run [build.bat](./build.bat)
--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ pesde run config
 pesde run sourcemap_generator > sourcemap.json
 stylua src
 selene src
-curl https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua -o ./global_types/roblox.d.luau
-luau_lsp analyze --settings .vscode/settings.json --definitions ./global_types/roblox.d.luau src
+curl https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua -o ./roblox.d.luau
+luau_lsp analyze --settings .vscode/settings.json --definitions ./roblox.d.luau src
 darklua process src/init.luau dist.luau -c .darklua.json
