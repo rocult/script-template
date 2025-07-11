99ab61aa91256d0159c5f34a84ecdc63fd80913c
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2025-07-11 13:30:01 +0100

    refactor: use `.meta`, formatting, reorganisatoin

--- build.bat
+++
@@ -1,7 +0,0 @@
-pesde run config
-pesde run sourcemap_generator > sourcemap.json
-stylua src
-selene src
-curl https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua -o ./roblox.d.luau
-luau_lsp analyze --settings .vscode/settings.json --definitions ./roblox.d.luau src
-darklua process src/init.luau dist.luau -c .darklua.json
