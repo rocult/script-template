db943d397ba0a5f68caa2fc9583ef3004ff0b846
Stefan <42220813+Stefanuk12@users.noreply.github.com>
2025-08-01 12:59:34 +0100

    chore: rename dist.luau to dist.lua

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,5 +3,5 @@ stylua src -f .meta/.stylua.toml
 selene src --config .meta/selene.toml
 curl https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua -o ./roblox.d.luau
 luau_lsp analyze --settings .vscode/settings.json --definitions ./roblox.d.luau src
-darklua process src/init.luau dist.luau -c .meta/.darklua.json
+darklua process src/init.luau dist.lua -c .meta/.darklua.json
 del roblox.d.luau
