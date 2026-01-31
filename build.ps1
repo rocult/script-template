171d9810cea89839427e061082cd9f6f6057be57
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2026-01-31 16:09:40 +0000

    chore: fix build script to align with new luau lsp stuff

--- build.ps1
+++ build.ps1
@@ -4,6 +4,6 @@ pesde run sourcemap_generator | Out-File .meta/sourcemap.json -Encoding utf8 # N
 stylua src -f .meta/.stylua.toml
 selene src --config .meta/selene.toml
 Invoke-WebRequest https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua -OutFile ./roblox.d.luau
-luau_lsp analyze --settings .vscode/settings.json --definitions ./roblox.d.luau src
+luau_lsp analyze --settings .vscode/settings.json --definitions "@roblox=./roblox.d.luau" --ignore /roblox_packages/** src
 darklua process src/init.luau dist.lua -c .meta/.darklua.json # This is the main thing, make sure you at least run this
 Remove-Item roblox.d.luau
